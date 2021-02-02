29c56e927b16794e70963856916380029b92e37c
patrickjolliffe <jolliffe@gmail.com>
2021-02-02 19:56:04 +0000

    Simplify

--- testcase.sql
+++ testcase.sql
@@ -5,7 +5,7 @@ as
 select rownum id, 
        rownum id2
 from dual 
-connect by level <= 1000;
+connect by level <= 10;
 
 alter table l add constraint l_id primary key(id);
 
@@ -16,8 +16,8 @@ drop table t1 purge;
 create table t1 nologging
 as
 select  ROWNUM id,
-        MOD(ROWNUM, 400) m
-from  dual connect by rownum <= 140000;
+        MOD(ROWNUM, 5) m
+from  dual connect by rownum <= 10;
 create index t1_m_id ON t1 (m, id);
 
 exec dbms_stats.gather_table_stats(null, 't1');
@@ -38,15 +38,13 @@ create index t2_c1_c2_id on t2(c1, c2, id);
 
 exec dbms_stats.gather_table_stats(null, 't2')
 
-var v1  NUMBER
-var v2  NUMBER
+var n  NUMBER
 
-exec :v1 := 999999;
-exec :v2 := 999999;
+exec :v := 1;
 
 explain plan for SELECT /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null FROM l, t1, t2 WHERE  
-(  ( t1.m = :v1 AND t2.c1 BETWEEN 21 AND 22)       OR 
-   ( t1.m = :v2 AND t2.c1 = 103 AND t2.c2 = 23  )   )  AND 
+(  ( t1.m = :n AND t2.c1 BETWEEN 11 AND 12)       OR 
+   ( t1.m = 1 AND t2.c1 = 103 AND t2.c2 = 13  )   )  AND 
     ( l.id = t1.id AND l.id2 = t2.id );
 
 select * from dbms_xplan.display();
