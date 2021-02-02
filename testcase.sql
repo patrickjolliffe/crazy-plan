4eda3c4c2c3a8afdd83953f2e86d229884e5af0a
patrickjolliffe <jolliffe@gmail.com>
2021-02-02 20:01:40 +0000

    Simplify

--- testcase.sql
+++ testcase.sql
@@ -27,8 +27,8 @@ drop table t2 purge;
 create table t2 nologging 
 as 
 select ROWNUM  id, 
-        MOD(ROWNUM, 10) c1,
-        MOD(ROWNUM, 10) c2
+        MOD(ROWNUM, 8) c1,
+        MOD(ROWNUM, 8) c2
 from    dual
 connect by level <= 10000;
 
@@ -38,12 +38,8 @@ create index t2_c1_c2_id on t2(c1, c2, id);
 
 exec dbms_stats.gather_table_stats(null, 't2')
 
-var n  NUMBER
-
-exec :v := 1;
-
 explain plan for SELECT /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null FROM l, t1, t2 WHERE  
-(  ( t1.m = :n AND t2.c1 BETWEEN 11 AND 12)       OR 
+(  ( t1.m = 0 AND t2.c1 BETWEEN 9 AND 10)       OR 
    ( t1.m = 1 AND t2.c1 = 103 AND t2.c2 = 13  )   )  AND 
     ( l.id = t1.id AND l.id2 = t2.id );
 
