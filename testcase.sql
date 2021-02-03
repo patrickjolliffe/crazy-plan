1c177a5cd210fd4d15b7ec789fdefc4a8691759c
patrickjolliffe <jolliffe@gmail.com>
2021-02-03 11:37:13 +0000

    Simplify, add trace

--- testcase.sql
+++ testcase.sql
@@ -2,12 +2,12 @@ drop table l purge;
 
 create table l nologging 
 as 
-select rownum id, 
-       rownum id2
+select rownum lid, 
+       rownum lc1
 from dual 
 connect by level <= 10;
 
-alter table l add constraint l_id primary key(id);
+alter table l add constraint l_lid primary key(lid);
 
 exec dbms_stats.gather_table_stats(null, 'l');
 
@@ -15,10 +15,10 @@ drop table t1 purge;
 
 create table t1 nologging
 as
-select  ROWNUM id,
-        MOD(ROWNUM, 5) m
+select  ROWNUM t1id,
+        MOD(ROWNUM, 5) t1m
 from  dual connect by rownum <= 10;
-create index t1_m_id ON t1 (m, id);
+create index t1_t1m_t1id ON t1 (t1m, t1id);
 
 exec dbms_stats.gather_table_stats(null, 't1');
 
@@ -26,21 +26,21 @@ drop table t2 purge;
 
 create table t2 nologging 
 as 
-select ROWNUM  id, 
-        MOD(ROWNUM, 8) c1,
-        MOD(ROWNUM, 8) c2
+select ROWNUM  t2id, 
+        MOD(ROWNUM, 8) t2c1,
+        MOD(ROWNUM, 8) t2c2
 from    dual
 connect by level <= 10000;
 
-alter table t2 add constraint t2_id primary key (id);
+alter table t2 add constraint t2_t2id primary key (t2id);
 
-create index t2_c1_c2_id on t2(c1, c2, id);
+create index t2_t2c1_t2c2_t2id on t2(t2c1, t2c2, t2id);
 
 exec dbms_stats.gather_table_stats(null, 't2')
 
 explain plan for SELECT /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null FROM l, t1, t2 WHERE  
-(  ( t1.m = 0 AND t2.c1 BETWEEN 9 AND 10)       OR 
-   ( t1.m = 1 AND t2.c1 = 103 AND t2.c2 = 13  )   )  AND 
-    ( l.id = t1.id AND l.id2 = t2.id );
+(  ( t1.t1m = 0 AND t2.t2c1 BETWEEN 9 AND 10)       OR 
+   ( t1.t1m = 1 AND t2.t2c1 = 14 AND t2.t2c2 = 13  )   )  AND 
+    ( l.lid = t1.t1id AND l.lc1 = t2.t2id );
 
 select * from dbms_xplan.display();
