64285e264af24adf43c2f8b1791aba80a542c95d
patrickjolliffe <jolliffe@gmail.com>
2022-10-04 12:07:36 +0100

    Stuff

---
+++ t.sql
@@ -0,0 +1,56 @@
+SET SQLFORMAT ANSICONSOLE
+
+alter system flush shared_pool;
+
+drop table l purge;
+
+create table l nologging 
+as 
+select rownum id 
+from dual 
+connect by level <= 10;
+
+alter table l add constraint l_pk primary key(id);
+
+exec dbms_stats.gather_table_stats(null, 'l');
+
+drop table t1 purge;
+
+create table t1 nologging
+as
+select  ROWNUM id,
+        mod(ROWNUM, 5) t1f1,
+        rpad('X', 255, 'X') padding
+from  dual connect by rownum <= 10;
+
+create index t1_t1f_id1 ON t1 (t1f1, id);
+
+exec dbms_stats.gather_table_stats(null, 't1');
+
+drop table t2 purge;
+
+create table t2 nologging 
+as 
+select ROWNUM  id, 
+        MOD(ROWNUM, 8) t2f1,
+        MOD(ROWNUM, 8) t2f2,
+        rpad('X', 255, 'X') padding
+from    dual
+connect by level <= 10000;
+
+alter table t2 add constraint t2_pk primary key (id);
+
+create index t2_t2f1_t2f2_id2 on t2(t2f1, t2f2, id);
+
+exec dbms_stats.gather_table_stats(null, 't2')
+
+
+explain plan for
+select /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null
+  from t1 join l  on t1.id = l.id join t2 on t2.id = l.id
+  where  ( ( t1.t1f1 = 0 AND t2.t2f1 = 0 AND t2.t2f2 = 15)       OR     
+           ( t1.t1f1 = 1 AND t2.t2f1 = 14 AND t2.t2f2 = 13  )   );
+
+select * from dbms_xplan.display(format=>'-predicate');
+
+exit;
--- t3.sql
+++ t3.sql
@@ -1,3 +1,8 @@
+SET SQLFORMAT ANSICONSOLE
+SET PAGESIZE 0
+
+alter system flush shared_pool;
+
 drop table l purge;
 
 create table l nologging 
@@ -12,14 +17,14 @@ exec dbms_stats.gather_table_stats(null, 'l');
 
 drop table t1 purge;
 
-create table t1 nologging
-as
+create table t1 (id number, t1f1 number);
+
+insert into t1 (id, t1f1) 
 select  ROWNUM id,
-        mod(ROWNUM, 5) t1f1,
-        rpad('X', 255, 'X') padding
+        mod(ROWNUM, 2) t1f1
 from  dual connect by rownum <= 10;
 
-create index t1_t1f_id1 ON t1 (t1f1, id);
+create index t1f1_id ON t1 (t1f1, id);
 
 exec dbms_stats.gather_table_stats(null, 't1');
 
@@ -29,22 +34,22 @@ create table t2 nologging
 as 
 select ROWNUM  id, 
         MOD(ROWNUM, 8) t2f1,
-        MOD(ROWNUM, 8) t2f2,
-        rpad('X', 255, 'X') padding
+        0 t2f2
 from    dual
 connect by level <= 10000;
 
 alter table t2 add constraint t2_pk primary key (id);
 
-create index t2_t2f1_t2f2_id2 on t2(t2f1, t2f2, id);
+create index t2f1_t2f2_id2 on t2(t2f1, t2f2, id);
 
-exec dbms_stats.gather_table_stats(null, 't2')
-
+exec dbms_stats.gather_table_stats(null, 't2');
 
 explain plan for
 select /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null
   from t1 join l  on t1.id = l.id join t2 on t2.id = l.id
-  where  ( ( t1.t1f1 = 0 AND t2.t2f1 = 0)       OR 
-           ( t1.t1f1 = 1 AND t2.t2f1 = 14 AND t2.t2f2 = 13  )   );
+  where  ( ( t1.t1f1 = 99 AND t2.t2f1 = 0                    ) OR 
+           ( t1.t1f1 =  0 AND t2.t2f1 = 99 AND t2.t2f2 = 99  )   );
 
 select * from dbms_xplan.display();
+
+exit;
