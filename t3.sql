266b6b725e8e65732b035bc2d14e31bb15e656fa
patrickjolliffe <jolliffe@gmail.com>
2022-10-10 12:31:11 +0100

    Improve

--- t3.sql
+++ t3.sql
@@ -47,8 +47,8 @@ exec dbms_stats.gather_table_stats(null, 't2');
 explain plan for
 select /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null
   from t1 join l  on t1.id = l.id join t2 on t2.id = l.id
-  where  ( ( t1.x = 99 AND t2.y = 0                    ) OR 
-           ( t1.x =  0 AND t2.y = 99 AND t2.z = 99  )   );
+  where  ( ( t1.x = 999 AND t2.y = 2                    ) OR 
+           ( t1.x =   1 AND t2.y = 999 AND t2.z = 3  )   );
 
 select * from dbms_xplan.display();
 
