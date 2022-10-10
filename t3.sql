b7ca6d7ffe3685cc2f7c3afdbb57368607455483
patrickjolliffe <jolliffe@gmail.com>
2022-10-10 12:25:44 +0100

    Rename columns

--- t3.sql
+++ t3.sql
@@ -17,14 +17,14 @@ exec dbms_stats.gather_table_stats(null, 'l');
 
 drop table t1 purge;
 
-create table t1 (id number, t1f1 number);
+create table t1 (id number, x number);
 
-insert into t1 (id, t1f1) 
-select  ROWNUM id,
-        mod(ROWNUM, 2) t1f1
+insert into t1 (id, x) 
+select  ROWNUM,
+        mod(ROWNUM, 2) 
 from  dual connect by rownum <= 10;
 
-create index t1f1_id ON t1 (t1f1, id);
+create index x_id ON t1 (x, id);
 
 exec dbms_stats.gather_table_stats(null, 't1');
 
@@ -33,22 +33,22 @@ drop table t2 purge;
 create table t2 nologging 
 as 
 select ROWNUM  id, 
-        MOD(ROWNUM, 8) t2f1,
-        0 t2f2
+        MOD(ROWNUM, 8) y,
+        0 z
 from    dual
 connect by level <= 10000;
 
 alter table t2 add constraint t2_pk primary key (id);
 
-create index t2f1_t2f2_id2 on t2(t2f1, t2f2, id);
+create index y_z_id on t2(y, z, id);
 
 exec dbms_stats.gather_table_stats(null, 't2');
 
 explain plan for
 select /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null
   from t1 join l  on t1.id = l.id join t2 on t2.id = l.id
-  where  ( ( t1.t1f1 = 99 AND t2.t2f1 = 0                    ) OR 
-           ( t1.t1f1 =  0 AND t2.t2f1 = 99 AND t2.t2f2 = 99  )   );
+  where  ( ( t1.x = 99 AND t2.y = 0                    ) OR 
+           ( t1.x =  0 AND t2.y = 99 AND t2.z = 99  )   );
 
 select * from dbms_xplan.display();
 
