c747977049975b5598156b6337d45447e66b46e5
patrickjolliffe <jolliffe@gmail.com>
2022-10-11 11:30:22 +0100

    Improvements

--- i.sql
+++ i.sql
@@ -1,8 +1,10 @@
 drop table l purge;
 
-create table l nologging 
-as 
-select rownum id 
+
+create table l (id number not null);
+
+insert into l (id) 
+select rownum
 from dual 
 connect by level <= 8;
 
@@ -12,30 +14,31 @@ exec dbms_stats.gather_table_stats(null, 'l');
 
 drop table t1 purge;
 
-create table t1 (id number, x number);
+create table t1 (id number, x2 number);
 
-insert into t1 (id, x) 
+insert into t1 (id, x2) 
 select  ROWNUM,
         mod(ROWNUM, 2) + 1
 from  dual connect by rownum <= 10;
 
-create index x_id ON t1 (x, id);
+create index x2_id ON t1 (x2, id);
 
 exec dbms_stats.gather_table_stats(null, 't1');
 
 drop table t2 purge;
 
-create table t2 nologging 
-as 
-select ROWNUM  id, 
-        MOD(ROWNUM, 8)+1 y,
-        1 z
+create table t2 (id number, y10 number, z1 number);
+
+insert into t2 (id, y10, z1) 
+select ROWNUM,
+       MOD(ROWNUM, 10)+1,
+       1
 from    dual
 connect by level <= 10000;
 
 alter table t2 add constraint t2_pk primary key (id);
 
-create index y_z on t2(y, z, id);
+create index y10_z1 on t2(y10, z1, id);
 
 exec dbms_stats.gather_table_stats(null, 't2');
 
--- t3.sql
+++ t3.sql
@@ -6,8 +6,8 @@ alter system flush shared_pool;
 explain plan for
 select /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null
   from t1 join l  on t1.id = l.id join t2 on t2.id = l.id
-  where  ( ( t1.x = 0 AND t2.y = 1               ) OR 
-           ( t1.x = 1 AND t2.y = 0 AND t2.z = 0  )   );
+  where  ( ( t1.x2 = 0 AND t2.y10 = 1               ) OR 
+           ( t1.x2 = 1 AND t2.y10 = 0 AND t2.z1 = 0  )   );
 
 select * from dbms_xplan.display();
 
