36b214cb1e67f94c85809c6767d5b71bf82dd288
patrickjolliffe <jolliffe@gmail.com>
2022-10-11 11:39:15 +0100

    Stuff

--- i.sql
+++ i.sql
@@ -1,45 +1,44 @@
-drop table l purge;
+drop table a purge;
 
+create table a (id number, m2 number);
 
-create table l (id number not null);
-
-insert into l (id) 
-select rownum
-from dual 
-connect by level <= 8;
-
-alter table l add constraint l_pk primary key(id);
-
-exec dbms_stats.gather_table_stats(null, 'l');
-
-drop table t1 purge;
-
-create table t1 (id number, x2 number);
-
-insert into t1 (id, x2) 
+insert into a (id, m2) 
 select  ROWNUM,
         mod(ROWNUM, 2) + 1
 from  dual connect by rownum <= 10;
 
-create index x2_id ON t1 (x2, id);
+create index x2_id ON a (m2, id);
 
-exec dbms_stats.gather_table_stats(null, 't1');
+exec dbms_stats.gather_table_stats(null, 'a');
 
-drop table t2 purge;
+drop table b purge;
 
-create table t2 (id number, y10 number, z1 number);
+create table b (id number not null);
 
-insert into t2 (id, y10, z1) 
+insert into b (id) 
+select rownum
+from dual 
+connect by level <= 8;
+
+alter table b add constraint l_pk primary key(id);
+
+exec dbms_stats.gather_table_stats(null, 'b');
+
+drop table c purge;
+
+create table c (id number, m10 number, m1 number);
+
+insert into c (id, m10, m1) 
 select ROWNUM,
        MOD(ROWNUM, 10)+1,
        1
 from    dual
 connect by level <= 10000;
 
-alter table t2 add constraint t2_pk primary key (id);
+alter table c add constraint t2_pk primary key (id);
 
-create index y10_z1 on t2(y10, z1, id);
+create index y10_z1 on c(m10, m1, id);
 
-exec dbms_stats.gather_table_stats(null, 't2');
+exec dbms_stats.gather_table_stats(null, 'c');
 
 exit;
--- t3.sql
+++ t3.sql
@@ -5,9 +5,9 @@ alter system flush shared_pool;
 
 explain plan for
 select /*+ OPT_PARAM('_optimizer_cbqt_or_expansion' 'off') */ null
-  from t1 join l  on t1.id = l.id join t2 on t2.id = l.id
-  where  ( ( t1.x2 = 0 AND t2.y10 = 1               ) OR 
-           ( t1.x2 = 1 AND t2.y10 = 0 AND t2.z1 = 0  )   );
+  from a join b  on b.id = a.id join c on c.id = b.id
+  where  ( ( a.m2 = 0 AND c.m10 = 1               ) OR 
+           ( a.m2 = 1 AND c.m10 = 0 AND c.m1 = 0  )   );
 
 select * from dbms_xplan.display();
 
